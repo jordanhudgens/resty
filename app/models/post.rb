a07daa164b9b509e6ff1fee4c89b806724745840
Jordan Hudgens <jordan@wow.com>
2015-10-29 17:27:13 -0500

    Updated a callback for the send sms features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  after_create :send_sms
+
+  def send_sms
+    `curl -X POST -d "notification[title]=#{self.title}" -d "notification[url]=http://edutechional-resty.herokuapp.com/posts/#{self.id}" http://edutechional-smsy.herokuapp.com/notifications`
+  end
 end
